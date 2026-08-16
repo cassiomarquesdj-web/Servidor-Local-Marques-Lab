347e2ba9ff461f0d7df6bdcda68d79ff084c72df
cassiomarquesdj-web <cassiomarquesdj@gmail.com>
2026-08-15 23:46:46 -0300

    feat(ui16): expand mixer state for full channel and bus metrics

--- ui16-iphone/Sources/UI16Controller/Models/UI16State.swift
+++ ui16-iphone/Sources/UI16Controller/Models/UI16State.swift
@@ -2,16 +2,39 @@ import Foundation
 
 struct UI16State: Equatable, Sendable {
     var connected = false
-    var masterLevel: Double = 0
+    var masterLevel: Double = 0.5
     var masterMuted = false
-    var channels: [Int: ChannelState] = [:]
+    var channels: [Int: ChannelState] = (1...16).reduce(into: [:]) { $0[$1] = ChannelState() }
+    var buses: [String: BusState] = [:]
+    var metrics: [String: String] = [:]
+    var meters: [String: Double] = [:]
+    var selectedBus = "master"
+    var lastMessage = ""
 
     struct ChannelState: Equatable, Sendable {
-        var name: String = ""
-        var level: Double = 0
+        var name = ""
+        var level = 0.5
         var muted = false
-        var pan: Double = 0
-        var gain: Double = 0
-        var highPass: Double = 0
+        var solo = false
+        var pan = 0.5
+        var gain = 0.5
+        var highPass = 0.0
+        var lowPass = 1.0
+        var phantom = false
+        var phase = false
+        var link = false
+        var eqEnabled = true
+        var gateEnabled = false
+        var compressorEnabled = false
+        var fxSend = 0.0
+        var auxSends: [Int: Double] = [:]
+    }
+
+    struct BusState: Equatable, Sendable {
+        var level = 0.5
+        var muted = false
+        var solo = false
+        var pan = 0.5
+        var sends: [Int: Double] = [:]
     }
 }
