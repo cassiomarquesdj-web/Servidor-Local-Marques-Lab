2187797c796f2847a8e960c0d8a26c1ccb107d91
cassiomarquesdj-web <cassiomarquesdj@gmail.com>
2026-08-15 23:47:15 -0300

    fix(ui16): expose state models to executable target

--- ui16-iphone/Sources/UI16Controller/Models/UI16State.swift
+++ ui16-iphone/Sources/UI16Controller/Models/UI16State.swift
@@ -1,40 +1,44 @@
 import Foundation
 
-struct UI16State: Equatable, Sendable {
-    var connected = false
-    var masterLevel: Double = 0.5
-    var masterMuted = false
-    var channels: [Int: ChannelState] = (1...16).reduce(into: [:]) { $0[$1] = ChannelState() }
-    var buses: [String: BusState] = [:]
-    var metrics: [String: String] = [:]
-    var meters: [String: Double] = [:]
-    var selectedBus = "master"
-    var lastMessage = ""
+public struct UI16State: Equatable, Sendable {
+    public var connected = false
+    public var masterLevel: Double = 0.5
+    public var masterMuted = false
+    public var channels: [Int: ChannelState] = (1...16).reduce(into: [:]) { $0[$1] = ChannelState() }
+    public var buses: [String: BusState] = [:]
+    public var metrics: [String: String] = [:]
+    public var meters: [String: Double] = [:]
+    public var selectedBus = "master"
+    public var lastMessage = ""
 
-    struct ChannelState: Equatable, Sendable {
-        var name = ""
-        var level = 0.5
-        var muted = false
-        var solo = false
-        var pan = 0.5
-        var gain = 0.5
-        var highPass = 0.0
-        var lowPass = 1.0
-        var phantom = false
-        var phase = false
-        var link = false
-        var eqEnabled = true
-        var gateEnabled = false
-        var compressorEnabled = false
-        var fxSend = 0.0
-        var auxSends: [Int: Double] = [:]
+    public init() {}
+
+    public struct ChannelState: Equatable, Sendable {
+        public var name = ""
+        public var level = 0.5
+        public var muted = false
+        public var solo = false
+        public var pan = 0.5
+        public var gain = 0.5
+        public var highPass = 0.0
+        public var lowPass = 1.0
+        public var phantom = false
+        public var phase = false
+        public var link = false
+        public var eqEnabled = true
+        public var gateEnabled = false
+        public var compressorEnabled = false
+        public var fxSend = 0.0
+        public var auxSends: [Int: Double] = [:]
+        public init() {}
     }
 
-    struct BusState: Equatable, Sendable {
-        var level = 0.5
-        var muted = false
-        var solo = false
-        var pan = 0.5
-        var sends: [Int: Double] = [:]
+    public struct BusState: Equatable, Sendable {
+        public var level = 0.5
+        public var muted = false
+        public var solo = false
+        public var pan = 0.5
+        public var sends: [Int: Double] = [:]
+        public init() {}
     }
 }
