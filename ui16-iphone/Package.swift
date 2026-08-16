2ccffe64f07e80cd422889c4f00ca97e0ac5ba6a
cassiomarquesdj-web <cassiomarquesdj@gmail.com>
2026-08-15 23:46:40 -0300

    build(ui16): make iOS app an executable Swift package target

--- ui16-iphone/Package.swift
+++ ui16-iphone/Package.swift
@@ -2,13 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "UI16Controller",
+    name: "UI16Phone",
     platforms: [.iOS(.v17)],
     products: [
+        .executable(name: "UI16Phone", targets: ["UI16Phone"]),
         .library(name: "UI16Controller", targets: ["UI16Controller"])
     ],
     targets: [
-        .target(name: "UI16Controller", path: "Sources/UI16Controller"),
+        .target(name: "UI16Controller", path: "Sources/UI16Controller", exclude: ["App"]),
+        .executableTarget(name: "UI16Phone", dependencies: ["UI16Controller"], path: "Sources/UI16Controller/App"),
         .testTarget(name: "UI16ProtocolTests", dependencies: ["UI16Controller"], path: "Tests")
     ]
 )
